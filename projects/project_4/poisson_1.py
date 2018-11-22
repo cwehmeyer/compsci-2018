2da1832501ac2a73e4f89e1309e132d4df250d7a
Felix Oertel <felix_oertel402@hotmail.com>
2018-11-22 18:40:01 +0100

    Added 2D-laplacian-script

---
+++ projects/project_4/poisson_1.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def create_laplacian_1d(nx, lx, pbc=True):
+    """Ceates a discretized Laplacian in 1D
+
+    Arguments:
+        nx (int): number of grid points; needs more than one
+        lx (float): box lenght along x; must be positive
+        pbc (boolean): use periodic boundary conditions
+    """
+    if nx < 2:
+        raise ValueError('We need at least two grid points')
+    if lx <= 0.0:
+        raise ValueError('We need a positive length')
+    if pbc not in (True, False):
+        raise TypeError('We need a boolean as pbc')
+    laplacian = np.zeros((nx, nx))
+    mx = (nx / lx)**2
+    for x in range(nx):
+        laplacian[x, x] -= 2.0 
+        laplacian[x, (x + 1) % nx] += 1
+        laplacian[(x + 1) % nx, x] += 1
+    if not pbc:
+        laplacian[0, -1] = 0
+        laplacian[-1, 0] = 0
+    return laplacian
+
+def create_laplacian_2d(nx, lx, ny, ly, pbc=True):
+    """Ceates a discretized Laplacian in 2D using Kronecker-products
+
+    Arguments:
+        nx (int): number of grid points in x-direction; needs more than one
+        ny (int): number of grid points in y-direction; needs more than one
+        lx (float): box lenght along x; must be positive
+        ly (float): box lenght along y; must be positive
+        pbc (boolean): use periodic boundary conditions
+    """
+    laplacian_x = create_laplacian_1d(nx, lx, pbc)
+    laplacian_y = create_laplacian_1d(ny, ly, pbc)
+    mx = (nx / lx)**2
+    my = (ny / ly)**2
+    ey, ex = np.identity(ny), np.identity(nx)
+    
+    laplacian = np.kron(laplacian_x, ey) + np.kron(ex, laplacian_y)
+
+    return laplacian
