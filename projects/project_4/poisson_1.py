c350503c049b3099acc1a4e76c668e46185c5477
Felix Oertel <felix_oertel402@hotmail.com>
2018-11-25 13:49:56 +0100

    Simplified poisson_1.py

--- projects/project_4/poisson_1.py
+++ projects/project_4/poisson_1.py
@@ -1,29 +1,5 @@
 import numpy as np
-
-def create_laplacian_1d(nx, lx, pbc=True):
-    """Ceates a discretized Laplacian in 1D
-
-    Arguments:
-        nx (int): number of grid points; needs more than one
-        lx (float): box lenght along x; must be positive
-        pbc (boolean): use periodic boundary conditions
-    """
-    if nx < 2:
-        raise ValueError('We need at least two grid points')
-    if lx <= 0.0:
-        raise ValueError('We need a positive length')
-    if pbc not in (True, False):
-        raise TypeError('We need a boolean as pbc')
-    laplacian = np.zeros((nx, nx))
-    mx = (nx / lx)**2
-    for x in range(nx):
-        laplacian[x, x] -= 2.0 
-        laplacian[x, (x + 1) % nx] += 1
-        laplacian[(x + 1) % nx, x] += 1
-    if not pbc:
-        laplacian[0, -1] = 0
-        laplacian[-1, 0] = 0
-    return laplacian
+from poisson import create_laplacian_2d
 
 def create_laplacian_2d(nx, lx, ny, ly, pbc=True):
     """Ceates a discretized Laplacian in 2D using Kronecker-products
